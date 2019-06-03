73811b1b2a981f4564b861158151b7fb96453be7
Ron Will <ron.will@virtualhighschool.com>
2019-06-03 13:50:34 -0400

    trying something new.

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,17 @@
 import EasyCm from './EasyCm.vue'
-const VueEasyCm  = {
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCaretRight)
+
+const VueEasyCm = {
   install: function (Vue) {
     Vue.component('easy-cm', EasyCm)
-    Vue.prototype.$easycm = function (e,root,tag) {
+    Vue.prototype.$easycm = function (e, root, tag) {
       e.stopPropagation()
       e.preventDefault()
-      root.$emit('easyAxis',{
+      root.$emit('easyAxis', {
         tag: tag,
         x: e.clientX,
         y: e.clientY
@@ -15,7 +21,8 @@ const VueEasyCm  = {
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(VueEasyCm)
+  window.Vue.use(VueEasyCm)
+  window.Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 }
 
 export default VueEasyCm
--- src/main.js
+++ src/main.js
@@ -1,12 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueEasyCm from './lib/index'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faCaretRight)
-Vue.component('FontAwesomeIcon', FontAwesomeIcon)
 
 Vue.use(VueEasyCm)
 new Vue({
