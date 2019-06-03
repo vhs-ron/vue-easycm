abb648f73d502949d67d524fbf67452e5ba51078
Ron Will <ron.will@virtualhighschool.com>
2019-06-03 14:11:08 -0400

    fixed icon issue.

--- src/icons/icons.js
+++ src/icons/icons.js
@@ -1,7 +1,7 @@
 
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import library from '@fortawesome/fontawesome-svg-core'
 import { faCaretRight } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faCaretRight)
